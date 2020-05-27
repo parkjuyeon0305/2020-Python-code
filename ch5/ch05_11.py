6adaf755d0c12d6aadc9f58f08b9b0b529308ce3
parkjuyeon0305 <65027146+parkjuyeon0305@users.noreply.github.com>
2020-05-27 17:41:55 +0900

    Add files via upload

---
+++ ch5/ch05_11.py
@@ -0,0 +1,17 @@
+a = []
+for i in range(10):
+    a.append(i)
+print(a)
+
+seq = [i for i in range(10)]
+print(seq)
+
+s = []
+for i in range(10):
+    if i%2 == 1:
+        s.append(i**2)
+print(s)
+
+squares = [i**2 for i in range(10) if i%2 == 1]
+print(squares)
+    
